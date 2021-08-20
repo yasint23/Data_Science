9b5523b1ee56a8709e42d0dfbfd0275504ef89df
pakyasin23@gmail.com <87154797+yasint23@users.noreply.github.com>
2021-08-20 14:08:25 +0100

    Python Exercises Updated and Libraries Works added

--- python/python_works/Assignment_Password_Reminder.py
+++
@@ -1,7 +0,0 @@
-name="yasin"
-user = input("Enter user name: ")
-if user == name:
-    print("Hello," + user + "! The password is : W@12")
-else:
-    print("Hello," + user + "! See you later.")
-    
--- python/python_works/Basic_Exercise.py
+++ python/python_works/Basic_Exercise.py
@@ -132,7 +132,12 @@ for i in range(1, 6):
 for i in range(1,10) :
     print(str(i) * i)
         
-
+name="yasin"
+user = input("Enter user name: ")
+if user == name:
+    print("Hello," + user + "! The password is : W@12")
+else:
+    print("Hello," + user + "! See you later.")
 
 
 
